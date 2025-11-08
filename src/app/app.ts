462919b2e31492f53ee30d080afaf60d3c8ee0eb
Marcus Coube <marcus.coube@gmail.com>
2025-11-08 15:49:32 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,15 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ecoprobe-landing-page');
+
+
+  openWhatsapp() {
+    window.open('https://wa.me/41987191306', '_blank');
+  }
 }
